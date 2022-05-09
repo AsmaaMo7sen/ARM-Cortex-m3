b2f01e126c901c23525c4a9a2ad2bd5147ffc342
asmaa mohsen <asma2mo7sen2000@gmail.com>
2022-05-09 23:52:44 +0200

    Led software driver

--- HAL/LED/LED_program.c
+++ HAL/LED/LED_program.c
@@ -14,8 +14,8 @@
 #include "STK_interface.h"
 #include "RCC_interface.h"
 #include "GPIO_interface.h"
+
 #include "LED_interface.h"
-#include "GPIO_private.h"
 #include "LED_private.h"
 #include "LED_config.h"
 
@@ -23,190 +23,19 @@
 
 void LED_voidLedOn(u8 COPY_u8Port ,u8 COPY_u8Pin)
 {
-	/*switching over ports*/
-	switch (COPY_u8Port)
-	{
-	    /*in case of port A*/
-		case GPIO_PORTA :
-
-			if (COPY_u8Pin <= 7)  //checking existing at first 8 pins
-			{
-				GPIOA->CRL &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOA->CRL |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOA->BSRR , COPY_u8Pin); //powering led on
-			}
-			else if (COPY_u8Pin <=15) //checking existing at second 8 pins
-			{
-				GPIOA->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOA->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOA->BSRR , COPY_u8Pin); //powering led on
-			}
-			else
-			{
-
-			}
-
-			/*break from case of port A*/
-			break ;
-
-		/*in case of port B*/
-		case GPIO_PORTB :
-			if (COPY_u8Pin <= 7)  //checking existing at first 8 pins
-			{
-				GPIOB->CRL &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOB->CRL |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOB->BSRR , COPY_u8Pin); //powering led on
-			}
-			else if (COPY_u8Pin <=15) //checking existing at second 8 pins
-			{
-				GPIOB->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOB->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOB->BSRR , COPY_u8Pin); //powering led on
-			}
-			else
-			{
-
-			}
-			/*break from case of port B*/
-			break ;
-
-		/*in case of port C*/
-		case GPIO_PORTC :
-
-		   if (COPY_u8Pin <=15) //checking existing at PORT C	 pins
-			{
-				GPIOC->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOC->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOC->BSRR , COPY_u8Pin); //powering led on
-			}
-			else
-			{
-
-			}
-			/*break from case of port C*/
-			break ;
-
-		/*in case of default*/
-		default :
-			/*do nothing and break*/
-			break ;
-	}
-
-	/*return from this function*/
-	return ;
+	GPIO_voidSetPinDirection(COPY_u8Port , COPY_u8Pin , GPIO_OUTPUT_PP_2M);
+	GPIO_voidSetOutputPinValue( COPY_u8Port , COPY_u8Pin , GPIO_HIGH);
 }
 
 void LED_voidLedOff(u8 COPY_u8Port ,u8 COPY_u8Pin)
-{	/*switching over ports*/
-	switch (COPY_u8Port)
-	{
-	    /*in case of port A*/
-		case GPIO_PORTA :
-
-			if (COPY_u8Pin <= 7)  //checking existing at first 8 pins
-			{
-				GPIOA->CRL &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOA->CRL |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOA->BRR , COPY_u8Pin); //powering led off
-			}
-			else if (COPY_u8Pin <=15) //checking existing at second 8 pins
-			{
-				GPIOA->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOA->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOA->BRR , COPY_u8Pin); //powering led off
-			}
-			else
-			{
-
-			}
-
-			/*break from case of port A*/
-			break ;
-
-		/*in case of port B*/
-		case GPIO_PORTB :
-			if (COPY_u8Pin <= 7)  //checking existing at first 8 pins
-			{
-				GPIOB->CRL &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOB->CRL |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOB->BRR , COPY_u8Pin); //powering led off
-			}
-			else if (COPY_u8Pin <=15) //checking existing at second 8 pins
-			{
-				GPIOB->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOB->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOB->BRR , COPY_u8Pin); //powering led off
-			}
-			else
-			{
-
-			}
-			/*break from case of port B*/
-			break ;
-
-		/*in case of port C*/
-		case GPIO_PORTC :
-
-			 if (COPY_u8Pin <=15) //checking existing at PORT C pins
-			{
-				GPIOC->CRH &= ~(0b1111 << (COPY_u8Pin *4)); //preparing pins to receive new value
-				GPIOC->CRH |= (0b0010 << (COPY_u8Pin *4)); //setting this pin to be output pushpull at 2MHz
-				SET_BIT(GPIOC->BRR , COPY_u8Pin); //powering led off
-			}
-			else
-			{
-
-			}
-			/*break from case of port C*/
-			break ;
-
-		/*in case of default*/
-		default :
-			/*do nothing and break*/
-			break ;
-
-	}
-	/*return from this function*/
-	return ;
+{
+	GPIO_voidSetPinDirection(COPY_u8Port , COPY_u8Pin , GPIO_OUTPUT_PP_2M);
+	GPIO_voidSetOutputPinValue( COPY_u8Port , COPY_u8Pin , GPIO_LOW);
 }
 
 void LED_voidLedToggle(u8 COPY_u8Port , u8 COPY_u8Pin)
 {
-	/*switching over ports*/
-		switch(COPY_u8Port)
-		{
-		    /*in case of port A*/
-			case GPIO_PORTA :
-
-				TOGGLE_BIT(GPIOA->ODR , COPY_u8Pin); //Toggling the pin value
-
-				/*break from case of port A*/
-				break ;
-
-		    /*in case of port B*/
-			case GPIO_PORTB :
-
-				TOGGLE_BIT(GPIOB->ODR , COPY_u8Pin); //Toggling the pin value
-
-				/*break from case of port B*/
-				break ;
-
-			/*in case of port C*/
-			case GPIO_PORTC :
-
-				TOGGLE_BIT(GPIOC->ODR , COPY_u8Pin); //Toggling the pin value
-
-				/*break from case of port C*/
-				break ;
-
-			default :
-				break ;
-		}
-
-
-    /*return from this function*/
-	return ;
-
+	GPIO_voidToggleLed(COPY_u8Port , COPY_u8Pin) ;
 }
 
 
